59034e82c3086e36752fb8e6fc430f9405374a4d
Ariell-jsj <king4r13l@gmail.com>
2026-04-24 21:04:01 -0500

    agregar bajarLimon

--- limones.js
+++ limones.js
@@ -1,13 +1,21 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
+
 const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=50;
 const ANCHO_PERSONAJE=30;
+const ANCHO_LIMON=20;
+const ALTURA_LIMON=20;
+
+
 let personajeX=canvas.width/2;
+let limonesX=canvas.width/2;
+let limonesY=5;
 
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimones();
 }
 
 //suelo y personaje
@@ -36,9 +44,22 @@ function actiualizarPantalla(){
     limpiarCanvas();
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimones();
+    
 }
 
 function limpiarCanvas(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
+
+}
+
+function dibujarLimones(){
+    ctx.fillStyle="green";
+    ctx.fillRect(limonesX,limonesY,ANCHO_LIMON,ALTURA_LIMON); 
+}
+
+function bajarLimones(){
+    limonesY = limonesY +10;
+    actiualizarPantalla(); 
 
 }
